64e3aaee95aecca508e99c43c3e4d10afcab420d
042cmy <042cmy@naver.com>
2020-12-13 00:58:56 +0900

    Sad

--- flaskProject_HomePage/app.py
+++ flaskProject_HomePage/app.py
@@ -86,7 +86,8 @@ def hi_world(phone,latitude,longnitude):
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return '가나다라'
+    #return render_template('index.html')
 
 
 if __name__ == '__main__':
