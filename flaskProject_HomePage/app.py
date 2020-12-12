f1a2da525153371af05d9fff6b60f7c437b6fe00
042cmy <042cmy@naver.com>
2020-12-13 01:23:17 +0900

    sdddd

--- flaskProject_HomePage/app.py
+++ flaskProject_HomePage/app.py
@@ -70,7 +70,7 @@ def hi_world(phone,latitude,longnitude):
     #res.status_code  # 응답 코드
     #res.raise_for_status()  # 200 OK 코드가 아닌 경우 에러 발동
 
-    area = pd_mag_data.values[0][1]
+    area = pd_mag_data.values[0][1].encode('raw_unicode_escape')
     #area = area.decode('raw_unicode_escape')
     #.decode('raw_unicode_escape')
     #area.encode('raw_unicode_escape')
