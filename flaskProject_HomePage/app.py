ad8209504696ac9c170316137183b73add11327b
042cmy <042cmy@naver.com>
2020-12-13 01:35:30 +0900

    Final

--- flaskProject_HomePage/app.py
+++ flaskProject_HomePage/app.py
@@ -70,7 +70,7 @@ def hi_world(phone,latitude,longnitude):
     res.status_code  # 응답 코드
     res.raise_for_status()  # 200 OK 코드가 아닌 경우 에러 발동
 
-    area = pd_mag_data.values[0][1].encode('raw_unicode_escape')
+    area = pd_mag_data.values[0][1]
 
      # json response일 경우 딕셔너리 타입으로 바로 변환
     data = {'sendMMS': res.json()['statusName'], 'sendTo': to, 'area': area,
